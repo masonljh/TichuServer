5b52480888edfb396b9df5bf53d680175e93b6c5
종효 이 <lee9212@smtown.com>
2019-05-03 18:59:05 +0900

    game 쪽 라운드 시작 함수 추가

--- game.js
+++ game.js
@@ -5,7 +5,12 @@ function Game(users) {
     this.bScore = 0;
     this.rounds = [];
     this.users = {};
+
     for (var id in users) {
+        if (this.turn === undefined) {
+            this.turn = id;
+        }
+
         this.users[id] = {};
         this.users[id].team = users[id].team;
     }
@@ -20,7 +25,10 @@ method.getCurrentRound = function() {
 }
 
 method.startRound = function() {
-
+    if (this.turn === undefined) {
+        this.turn = this.users;
+    }
+    this.rounds.push(new Round(this.users));
 };
 
 method.raiseCards = function(id, cards) {
